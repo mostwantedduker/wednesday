1adb92fbcadcc1cadcd70f97aecd7894d8ba3c5e
mostwantedduker <vicky.sks.1995@gmail.com>
2021-10-08 11:29:25 +0530

    alternate

---
+++ src/org/sample6/Swiggy.java
@@ -0,0 +1,21 @@
+package org.sample6;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Swiggy {
+public static void main(String[] args) {
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\LENOVO PC\\eclipse-workspace\\sample6\\driver");
+	WebDriver driver = new ChromeDriver();
+	
+	driver.get(" https://www.swiggy.com");
+	WebElement txtlocation = driver.findElement(By.id("Location"));
+	txtlocation.sendKeys("Madipakkam");
+    driver.quit();
+    
+    
+   
+}
+}
